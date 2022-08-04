9ad7a14a725b6b038445f015751284bf4e5ab734
yoitzlian <108893669+yoitzlian@users.noreply.github.com>
2022-08-04 13:21:31 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,30 @@
+hand = 0
+
+def on_gesture_shake():
+    global hand
+    hand = randint(1, 3)
+    if hand == 1:
+        basic.show_leds("""
+            # # # # #
+                        # . . . #
+                        # . . . #
+                        # . . . #
+                        # # # # #
+        """)
+    elif hand == 2:
+        basic.show_leds("""
+            . . . . .
+                        . # # # .
+                        . # # # .
+                        . # # # .
+                        . . . . .
+        """)
+    else:
+        basic.show_leds("""
+            # # . . #
+                        # # . # .
+                        . . # . .
+                        # # . # .
+                        # # . . #
+        """)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
